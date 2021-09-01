4675f58101b9cffb494ce8b2574f6a9549debfbf
Ilya Tyagin <tyagin@udel.edu>
2021-09-01 17:16:36 -0400

    added 'download_results' file (check previous commit)

---
+++ download_results.js
@@ -0,0 +1,21 @@
+function str_to_txt(source) {
+    return source.data['text_data']
+}
+
+
+const filename = 'agatha_semviz.txt'
+const filetext = str_to_txt(source)
+const blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
+
+//addresses IE
+if (navigator.msSaveBlob) {
+    navigator.msSaveBlob(blob, filename)
+} else {
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob)
+    link.download = filename
+    link.target = '_blank'
+    link.style.visibility = 'hidden'
+    link.dispatchEvent(new MouseEvent('click'))
+}
+
